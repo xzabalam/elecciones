fb87a903d64d8cbbebab1ec6046740818838c1f7
Xavier Zabala M <xzabalam@gmail.com>
2023-01-28 09:56:00 -0500

    Correcciones a los tres aplicativos

--- elecciones-main/src/main/java/com/eleccciones/cliente/data/repositories/security/UsuarioRepository.java
+++ elecciones-main/src/main/java/com/eleccciones/cliente/data/repositories/security/UsuarioRepository.java
@@ -2,6 +2,8 @@ package com.eleccciones.cliente.data.repositories.security;
 
 import com.eleccciones.cliente.data.entities.security.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,4 +18,18 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByUsername(String username);
+
+    @Query("select u " +
+            "from Usuario u " +
+            "join u.contrato c " +
+            "where u.username = :username " +
+            "and u.activo = 'S' " +
+            "and u.habilitadoMovil = 'S' " +
+            "and u.estado != 'D' " +
+            "and c.activo = 'S' " +
+            "and c.fechaFin >= current_timestamp " +
+            "and (c.sesionesActivas+1)< c.sesiones " +
+            "and c.estado != 'D' " +
+            "")
+    Optional<Usuario> getByUsername(@Param("username") String username);
 }
